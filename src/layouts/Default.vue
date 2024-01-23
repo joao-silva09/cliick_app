60ab1bc0414bfb41f074938cd6420609f5499a43
joao-silva09 <joao.e.silva.03@gmail.com>
2024-01-23 14:01:20 -0300

    Created NavHeader layout.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,10 +3,13 @@
     <Header />
     <div class="flex">
       <Sidebar />
-      <main class="flex-1 p-4">
-        <!-- Conteúdo do Dashboard vai aqui -->
-        <router-view />
-      </main>
+      <div class="flex-1">
+        <NavHeader />
+        <main class="flex-1 p-4">
+          <!-- Conteúdo do Dashboard vai aqui -->
+          <router-view />
+        </main>
+      </div>
     </div>
   </div>
 </template>
@@ -14,11 +17,13 @@
 <script>
 import Header from "./Header.vue";
 import Sidebar from "./Sidebar.vue";
+import NavHeader from "./NavHeader.vue";
 
 export default {
   components: {
     Header,
     Sidebar,
+    NavHeader
   },
 };
 </script>
---
+++ src/layouts/NavHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="bg-blue-100 w-full">section nav</nav>
+</template>
+
+<script>
+export default {
+  name: "NavHeader",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
