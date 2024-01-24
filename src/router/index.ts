0415ea3761d7a3d83a47e110b09815a0ae5248db
joao-silva09 <joao.e.silva.03@gmail.com>
2024-01-24 15:33:37 -0300

    Console fix.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,6 @@ import { routes } from "./routes";
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior() {
-    return {
-      el: "#main",
-      top: 0,
-    };
-  },
 });
 
 export default router;
