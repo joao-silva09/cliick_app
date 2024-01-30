2b965e532587b37b473297bf95d109b10e052be9
joao-silva09 <joao.e.silva.03@gmail.com>
2024-01-30 14:05:37 -0300

    Updated MyTasks view.

--- src/views/Demands/MyTasks.vue
+++ src/views/Demands/MyTasks.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <div class="flex justify-between items-center mb-3">
-      <h2 class="text-xl">minhas tarefas</h2>
-      <button class="bg-blue-600 text-white py-2 px-3 rounded hover:bg-blue-800">Criar Tarefa</button>
+      <h2 class="text-xl">Minhas Tarefas</h2>
     </div>
     <div class="flex gap-3 flex-col">
       <div
         v-for="task in $pinia.state.value.task.tasks"
+        :class="
+          new Date(task.deadline) > new Date()
+            ? 'bg-cyan-100'
+            : 'bg-red-200'
+        "
         class="w-full border rounded border-gray-600 p-2 bg-cyan-100 flex justify-between cursor-pointer hover:translate-x-2 hover:transition-all"
         @click.stop.prevent="getTask(task.id)"
       >
@@ -16,9 +20,6 @@
         </div>
         <div>
           {{ new Date(task.deadline).toLocaleDateString() }}
-        </div>
-        <div>
-          {{ task.demand.title }}
         </div>
       </div>
     </div>
