8a359e4123af40bcc8a922a38d5b0e5540987705
joao-silva09 <joao.e.silva.03@gmail.com>
2024-02-03 00:20:16 -0300

    Updated Task view.

--- src/views/Tasks/Task.vue
+++ src/views/Tasks/Task.vue
@@ -1,7 +1,7 @@
 <template>
   <BackIcon @click="goBack()" class="w-5 h-5 text-blue-600 cursor-pointer" />
   <div class="grid grid-cols-2 gap-2 pt-5">
-    <div class="w-full flex flex-col gap-8">
+    <div class="w-full flex flex-col gap-5">
       <div class="w-full border border-blue-700 p-3 rounded shadow-xl">
         <div class="flex justify-between items-center">
           <h2 class="text-xl">
@@ -22,31 +22,48 @@
             />
           </div>
         </div>
-        <h4>
-          {{ $pinia.state.value.task.task.description }}
-        </h4>
-        <h5>
-          {{ $pinia.state.value.task.task.status }}
-        </h5>
-        <p>
-          {{ $pinia.state.value.task.task.demand.customer.name }}
+        <p class="text-sm">
+          <b>Descrição: </b>{{ $pinia.state.value.task.task.description }}
         </p>
-
+        <h5><b>Status: </b>{{ $pinia.state.value.task.task.status }}</h5>
         <p>
-          Prazo:
+          <b>Cliente: </b
+          >{{ $pinia.state.value.task.task.demand.customer.name }}
+        </p>
+        <p>
+          <b>Prazo: </b>
           {{
             new Date($pinia.state.value.task.task.deadline).toLocaleDateString()
           }}
+        </p>
+        <p>
+          <b>Criado por: </b>
+          {{ $pinia.state.value.task.task.created_by }}
         </p>
       </div>
 
       <!-- <RequestApprovalTaskModal ref="requestApprovalTaskModal" /> -->
       <CompleteTaskModal ref="completeTaskModal" />
 
-      <div class="w-full border border-blue-700 p-3 rounded shadow-xl">
-        <h2>Atribuído a:</h2>
-        <div v-for="user in $pinia.state.value.task.task.users" :key="user.id">
-          {{ user.first_name + " " + user.last_name }}
+      <div class="grid grid-cols-2 gap-5">
+        <div class="w-full border border-blue-700 p-3 rounded shadow-xl">
+          <h2 class="font-bold">Times Envolvidos:</h2>
+          <div
+            v-for="team in $pinia.state.value.task.task.demand.teams"
+            :key="team.id"
+          >
+            {{ team.name }}
+          </div>
+        </div>
+
+        <div class="w-full border border-blue-700 p-3 rounded shadow-xl">
+          <h2 class="font-bold">Atribuído a:</h2>
+          <div
+            v-for="user in $pinia.state.value.task.task.users"
+            :key="user.id"
+          >
+            {{ user.first_name + " " + user.last_name }}
+          </div>
         </div>
       </div>
     </div>
@@ -54,7 +71,7 @@
     <!-- h-screen rounded-sm p-4 flex flex-1 flex-col justify-end overflow-y-auto -->
     <div>
       <div
-        class="px-3 messages h-[75vh] overflow-y-auto bg-sky-100 rounded p-2"
+        class="px-3 messages h-[68vh] overflow-y-auto bg-sky-100 rounded p-2"
         ref="messageContainer"
       >
         <div
@@ -84,7 +101,7 @@
         type="text"
         placeholder="Digite sua mensagem..."
         class="border mt-1 border-gray-400 shadow-xl w-full p-2 rounded focus:outline-none"
-        @keyup.enter="addMessage()"
+        @keyup.enter="sendMessage()"
       />
     </div>
   </div>
@@ -103,6 +120,7 @@ import {
   CheckCircleIcon,
   ArrowLeftIcon as BackIcon,
 } from "@heroicons/vue/24/outline";
+import { useApplicationStore } from "../../stores/ApplicationStore";
 
 const userStore = useUserStore();
 const taskStore = useTaskStore();
@@ -147,11 +165,14 @@ export default {
       this.$refs.completeTaskModal.openModal();
     },
 
-    addMessage() {
+    sendMessage() {
+      useApplicationStore().setIsLoading(true);
+
       const payload = {
         message: this.contentMessage,
         task_id: taskStore.task.id,
       };
+
       api
         .post("messages", payload)
         .then((response) => {
@@ -161,7 +182,10 @@ export default {
           // this.messagesList.push(response.data.data);
         })
         .catch((e) => alert(e))
-        .finally(() => this.scrollToBottom());
+        .finally(() => {
+          this.scrollToBottom();
+          useApplicationStore().setIsLoading(false);
+        });
 
       this.contentMessage = "";
     },
