a9582634d89f084e3c859a61fb78b151c1564ec0
joao-silva09 <joao.e.silva.03@gmail.com>
2024-03-01 11:03:25 -0300

    Added spinner in Login Button.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -32,27 +32,27 @@
               autocomplete="email"
               required=""
               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+            />
           </div>
-          
-          <div>
-            <div class="flex items-center justify-between">
-              <label
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label
               for="password"
               class="block text-sm font-medium leading-6 text-gray-900"
               >Senha</label
-              >
-              <div class="text-sm">
-                <a
+            >
+            <div class="text-sm">
+              <a
                 href="#"
                 class="font-semibold text-[#148] hover:text-indigo-500"
                 >Forgot password?</a
-                >
-              </div>
+              >
             </div>
-            <div class="mt-2">
-              <input
+          </div>
+          <div class="mt-2">
+            <input
               v-model="password"
               id="password"
               name="password"
@@ -60,17 +60,23 @@
               autocomplete="current-password"
               required=""
               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+            />
           </div>
-          
-          <div>
-            <button
+        </div>
+
+        <div>
+          <button
             @click.stop.prevent="login()"
             class="flex w-full justify-center rounded-md bg-[#148] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#1B4B73] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             :disabled="spinner.login"
           >
-            Sign in
+            <img
+              v-if="spinner.login"
+              src="../assets/img/spinner.svg"
+              alt=""
+              class="w-5 h-5 mr-2"
+            />
+            Entrar
           </button>
         </div>
       </form>
