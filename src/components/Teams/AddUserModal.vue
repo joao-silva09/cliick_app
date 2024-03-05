117efafa0fad54f3d113b625ad7fa82d562cfed3
joao-silva09 <joao.e.silva.03@gmail.com>
2024-03-05 16:14:04 -0300

    Updated form style.

--- src/components/Teams/AddUserModal.vue
+++ src/components/Teams/AddUserModal.vue
@@ -27,7 +27,7 @@
             v-model="users"
             :options="$pinia.state.value.team.usersToAdd"
             label="full_name"
-            class="bg-gray-300 mb-2"
+            class="px-2 block w-full rounded-md border-0 py-1.5 mb-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             multiple
             id="users"
             @update:modelValue="handleAddUser"
