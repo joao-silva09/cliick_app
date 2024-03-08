89421da803d10348bda1ffd6f733f195825f39a8
joao-silva09 <joao.e.silva.03@gmail.com>
2024-03-08 15:31:35 -0300

    Added profile photo in Header.

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -3,6 +3,20 @@
   <header class="bg-blue-50 p-4 flex justify-between items-center h-[10vh]">
     <!-- Conteúdo do cabeçalho vai aqui -->
     <img src="../assets/img/logo.png" class="w-16" />
+    <router-link :to="{ name: 'profile' }">
+      <img
+        v-if="$pinia.state.value.user.user.profile_photo"
+        :src="`http://localhost:8000/storage/${$pinia.state.value.user.user.profile_photo}`"
+        alt="Profile Photo"
+        class="w-11 rounded-full mb-2 cursor-pointer"
+      />
+      <img
+        v-else
+        src="../../assets/img/default-profile-photo.png"
+        alt="Profile Photo"
+        class="w-11 rounded-full mb-2 cursor-pointer"
+      />
+    </router-link>
   </header>
 </template>
 
@@ -10,7 +24,11 @@
 import cookie from "../services/cookie";
 export default {
   name: "Header",
-  methods: {},
+  methods: {
+    goToProfile() {
+      $router.push("/profile");
+    },
+  },
 };
 </script>
 
