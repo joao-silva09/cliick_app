128c3b979ce065a918d7174e258be3c70361e4c9
joao-silva09 <joao.e.silva.03@gmail.com>
2024-03-28 17:08:34 -0300

    Updated Demand list to Task list in Customer type.

--- src/types/Customer.ts
+++ src/types/Customer.ts
@@ -1,9 +1,9 @@
-import { Demand } from "./Demand";
+import { Task } from "./Task";
 
 export type Customer = {
   id?: Number;
   name?: String;
   email?: String;
   entry_date?: Date;
-  demands?: Array<Demand>;
+  tasks?: Array<Task>;
 };
