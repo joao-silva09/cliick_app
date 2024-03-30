e493f994922847da9de208add3f788037caacbbf
joao-silva09 <joao.e.silva.03@gmail.com>
2024-03-30 01:35:22 -0300

    Created phone input in AddCustomerModal.

--- src/components/Customers/AddCustomerModal.vue
+++ src/components/Customers/AddCustomerModal.vue
@@ -35,6 +35,14 @@
             class="px-2 block w-full rounded-md border-0 py-1.5 mb-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
 
+          <label for="phone">Telefone</label>
+          <input
+            v-model="customer.phone"
+            type="number"
+            id="phone"
+            class="px-2 block w-full rounded-md border-0 py-1.5 mb-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+
           <label for="entry_date">Data de Entrada</label>
           <input
             v-model="customer.entryDate"
@@ -77,6 +85,7 @@ export default {
       customer: {
         name: "",
         email: "",
+        phone: "",
         entryDate: "",
       },
     };
@@ -95,6 +104,7 @@ export default {
       const payload = {
         name: this.customer.name,
         email: this.customer.email,
+        phone: this.customer.phone.toString(),
         entry_date: this.customer.entryDate,
         company_id: 1,
       };
