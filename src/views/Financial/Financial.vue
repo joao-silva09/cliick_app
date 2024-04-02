ecc113c28b28b5b32038c9b32c24dc9404890df3
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-02 19:19:41 -0300

    Updated Financial folder structure.

--- src/views/Financial/Financial.vue
+++ src/views/Financial/Financial.vue
@@ -1,28 +1,13 @@
 <template>
   <div style="height: 300px">
     <h1 class="text-center mt-5">Financeiro</h1>
-
-    <ul>
-      <li v-for="team in teams" :key="team.id" class="bg-gray-100 my-2">
-        {{ team.name }} - {{ team.email }} - {{ team.entry_date }}
-      </li>
-    </ul>
-
-    <button
-      @click="openModal()"
-      class="px-4 py-2 bg-blue-500 text-white rounded-md"
-    >
-      Adicionar Time
-    </button>
-
-    <Modal ref="modal" />
   </div>
 </template>
 
 <script>
-import api from "../services/api";
-import { useTeamStore } from "../stores/TeamStore";
-import Modal from "../components/Teams/AddTeamModal.vue";
+import api from "../../services/api";
+import { useTeamStore } from "../../stores/TeamStore";
+import Modal from "../../components/Teams/AddTeamModal.vue";
 
 const teamStore = useTeamStore();
 export default {
