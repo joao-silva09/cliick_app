ad7c54b93fccb85e6a3884c999ab6cc59661dabf
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-04 18:03:49 -0300

    Updated store methods of expenses for write off company balance.

--- src/stores/ExpenseStore.ts
+++ src/stores/ExpenseStore.ts
@@ -2,21 +2,25 @@ import { defineStore } from "pinia";
 import api from "../services/api";
 import { useApplicationStore } from "./ApplicationStore";
 import { Expense } from "../types/Expense";
+import { useCompanyStore } from "./CompanyStore";
 
 export const useExpenseStore = defineStore("expense", {
   state: () => ({
     expenses: [] as Expense[],
+    totalExpense: 0
   }),
   actions: {
-    storeExpenses(esxpenses) {
-      this.expenses = esxpenses;
+    storeExpenses(expenses, totalExpense) {
+      this.expenses = expenses;
+      this.totalExpense = totalExpense;
+      useCompanyStore().storeCompany(expenses[0].company);
     },
     getExpenses() {
       useApplicationStore().setIsLoading(true);
       api
         .get("expenses")
         .then((response) => {
-          this.storeExpenses(response.data.data);
+          this.storeExpenses(response.data.expenses, response.data.totalExpense);
         })
         .catch((e) => alert(e))
         .finally(() => useApplicationStore().setIsLoading(false));
