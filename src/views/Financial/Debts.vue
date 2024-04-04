4fb48d2f8dbc72d014f386a274c5ae53561fa931
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-04 17:56:26 -0300

    Added Debts page.

---
+++ src/views/Financial/Debts.vue
@@ -0,0 +1,45 @@
+<template>
+  <div style="height: 300px">
+    <h1 class="text-center mt-5">Dívidas</h1>
+  </div>
+</template>
+
+<script>
+import api from "../../services/api";
+import { useTeamStore } from "../../stores/TeamStore";
+import Modal from "../../components/Teams/AddTeamModal.vue";
+
+const teamStore = useTeamStore();
+export default {
+  name: "Debts",
+
+  components: {
+    Modal,
+  },
+
+  data() {
+    return {
+      teams: [],
+    };
+  },
+
+  created() {
+    this.getTeams();
+  },
+
+  methods: {
+    getTeams() {
+      api.get("teams").then((response) => {
+        this.teams = response.data.data;
+        teamStore.storeTeams(response.data.data);
+      });
+    },
+
+    openModal() {
+      this.$refs.modal.openModal();
+    },
+  },
+};
+</script>
+
+<style></style>
