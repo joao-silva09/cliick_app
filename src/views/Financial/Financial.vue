474aba58a88b1a45c986383c0b9528c76ed0f09d
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-06 00:03:22 -0300

    Added expenses and revenue cards in Financial overview.

--- src/views/Financial/Financial.vue
+++ src/views/Financial/Financial.vue
@@ -1,42 +1,49 @@
 <template>
   <div style="height: 300px">
-    <h1 class="text-center mt-5">Financeiro</h1>
+    <div class="flex gap-5 text-white">
+      <div class="bg-green-600 w-[40%] rounded p-4 shadow-md">
+        <h4 class="font-semibold text-xl mb-6">Ganhos</h4>
+
+        <p class="text-4xl font-bold">R$ 4.000,00</p>
+      </div>
+      <div class="bg-red-600 w-[40%] rounded p-4 shadow-md">
+        <h4 class="font-semibold text-xl mb-4">Gastos</h4>
+
+        <p class="text-4xl font-bold">
+          {{ BRLFormat($pinia.state.value.expense.totalExpense) }}
+        </p>
+      </div>
+      <div class="bg-gray-50 w-[40%] rounded p-4 shadow-md"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import api from "../../services/api";
-import { useTeamStore } from "../../stores/TeamStore";
-import Modal from "../../components/Teams/AddTeamModal.vue";
+import { useExpenseStore } from "../../stores/ExpenseStore";
 
-const teamStore = useTeamStore();
+const expenseStore = useExpenseStore();
 export default {
   name: "Financial",
 
-  components: {
-    Modal,
-  },
-
   data() {
-    return {
-      teams: [],
-    };
+    return {};
   },
 
   created() {
-    this.getTeams();
+    this.getExpenses();
   },
 
   methods: {
-    getTeams() {
-      api.get("teams").then((response) => {
-        this.teams = response.data.data;
-        teamStore.storeTeams(response.data.data);
-      });
+    getExpenses() {
+      expenseStore.getExpenses();
     },
 
-    openModal() {
-      this.$refs.modal.openModal();
+    BRLFormat(amount) {
+      return amount.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL",
+      });
     },
   },
 };
