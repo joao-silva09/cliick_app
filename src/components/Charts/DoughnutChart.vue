393b565fa65af4d081bf1fc4eea20441131b51d3
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-10 00:02:19 -0300

    Added DoughnutChart component.

---
+++ src/components/Charts/DoughnutChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <Doughnut :data="data" :options="options" />
+</template>
+
+<script lang="ts">
+import { Chart as ChartJS, registerables } from "chart.js";
+import { Doughnut } from "vue-chartjs";
+
+ChartJS.register(...registerables);
+
+export default {
+  name: "DoughnutChart",
+  props: {
+    labels: Array,
+    data: Array,
+    colors: Array,
+  },
+  components: {
+    Doughnut,
+  },
+  data() {
+    return {
+      data: {
+        labels: this.$props.labels,
+        datasets: [
+          {
+            data: this.$props.data,
+            backgroundColor: this.$props.colors,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+      },
+    };
+  },
+};
+</script>
