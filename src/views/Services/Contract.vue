87cdea0245b68529e6c081e019804973c2f5d2e0
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-19 16:38:56 -0300

    Created Contract page.

--- src/views/Services/Contract.vue
+++ src/views/Services/Contract.vue
@@ -2,13 +2,31 @@
   <div>
     <BackIcon @click="goBack()" class="w-5 h-5 text-blue-600 cursor-pointer" />
 
+    <div>
+      <h3 class="text-lg text-center font-semibold">
+        Contrato de {{ contract.customer.name }}
+      </h3>
+
+      <img
+        v-if="contract.customer.customer_logo"
+        :src="`${baseUrl}/storage/${contract.customer.customer_logo}`"
+        alt="Profile Photo"
+        class="w-32 h-32 rounded-full mb-2 cursor-pointer"
+        @click="openUpdateProfilePhotoModal()"
+      />
+      <img
+        v-else
+        src="../assets/img/default-profile-photo.png"
+        alt="Profile Photo"
+        class="w-32 h-32 rounded-full mb-2 cursor-pointer"
+        @click="openUpdateProfilePhotoModal()"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { useContractStore } from "../../stores/ContractStore";
-import { ContractStatus } from "../../types/Enums";
-import AddContractModal from "../../components/Services/Contracts/AddContractModal.vue";
 import api from "../../services/api.ts";
 import { useApplicationStore } from "../../stores/ApplicationStore";
 import { ArrowLeftIcon as BackIcon } from "@heroicons/vue/24/outline";
@@ -18,19 +36,17 @@ export default {
   name: "Contracts",
 
   components: {
-    AddContractModal,
     BackIcon,
   },
 
   data() {
     return {
-      totalSpend: 0,
+      contract: this.$pinia.state.value.contract.contract,
       baseUrl: import.meta.env.VITE_API_BASE_URL,
     };
   },
 
-  created() {
-  },
+  created() {},
 
   methods: {
     goBack() {
