2ae7f3fc7a4c23655563df6d87062588eeb11921
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-21 22:48:37 -0300

    Created contract state and storeContract method.

--- src/stores/ContractStore.ts
+++ src/stores/ContractStore.ts
@@ -3,9 +3,14 @@ import { Contract } from "../types/Contract";
 
 export const useContractStore = defineStore("contract", {
   state: () => ({
+    contract: {} as Contract,
     contracts: [] as Contract[],
   }),
   actions: {
+    storeContract(contract) {
+      this.contract = contract;
+    },
+
     storeContracts(contracts) {
       this.contracts = contracts;
     },
